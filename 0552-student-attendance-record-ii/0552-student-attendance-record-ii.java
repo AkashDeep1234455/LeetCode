f9acf9853dfb600e99be518b682d1d887b737c14
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2024-05-27 22:07:04 +0530

    Time: 227 ms (69.54%), Space: 58.1 MB (64.16%) - LeetHub

---
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    static int mod = 1000000007;
+    public int checkRecord(int n) {
+        int dp[][][] = new int[n+1][2][3];
+        dp[0][0][0] = 1;
+        for(int i=1;i<=n;i++){
+            for(int a=0;a<2;a++){
+                for(int l=0;l<3;l++){
+                    dp[i][a][0] =(dp[i][a][0]+dp[i-1][a][l])%mod;
+                    if(a>0){
+                        dp[i][a][0] =(dp[i][a][0]+dp[i-1][a-1][l])%mod;
+                    }
+                    if(l>0){
+                        dp[i][a][l] = (dp[i][a][l]+dp[i-1][a][l-1])%mod;
+                    }
+                }
+            }
+        }
+        int result = 0;
+        for(int a=0;a<2;a++){
+            for(int l=0;l<3;l++){
+                result=(result+dp[n][a][l])%mod;
+            }
+        }
+        return result;
+    }
+}
