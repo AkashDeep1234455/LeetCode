f29c958e7b568fe307a0b5c841a381021cfe521f
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2024-07-10 17:42:08 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.61%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -11,39 +11,22 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
         if(head==null||head.next==null) return head;
-        Queue<ListNode> small = new LinkedList<>();
-        Queue<ListNode> big = new LinkedList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            if(temp.val<x) small.add(temp);
-            else big.add(temp);
-            temp = temp.next;
-        }
-        head = null;
-        ListNode prev = null;
-        ListNode curr = head;
-        while(!small.isEmpty()){
-            ListNode newNode = small.poll();
-            if(head==null) {
-                head = newNode;
-                curr = head;
-                continue;
-            }
-            curr.next = newNode;
-            prev = curr;
-            curr = newNode;
-        }
-        while(!big.isEmpty()){
-            ListNode newNode = big.poll();
-            if(head==null){
-                head = newNode;
-                curr = head;
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        ListNode leftHand = left;
+        ListNode rightHand = right;
+        while(head!=null){
+            if(head.val<x){
+                leftHand.next = head;
+                leftHand = leftHand.next;
             }else{
-                curr.next = newNode;
-                curr = newNode; 
+                rightHand.next = head;
+                rightHand = rightHand.next;
             }
+            head = head.next;
         }
-        curr.next = null;
-        return head;
+        leftHand.next = right.next;
+        rightHand.next = null;
+        return left.next;
     }
 }
