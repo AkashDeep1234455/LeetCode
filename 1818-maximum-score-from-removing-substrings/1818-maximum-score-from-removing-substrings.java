ac4b5040ba56790067bd69841cc4def15f73fcf3
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2024-07-12 15:52:35 +0530

    Time: 298 ms (16.16%), Space: 56 MB (6.06%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -5,49 +5,49 @@ class Solution {
         int yFirst = 0;
 
         //Stack for handling string removal
-        Stack<Character> stringStore = new Stack<>();
+        //this will handle x first
+        Stack<Character> stringStoreX = new Stack<>();
+        //this will handle y first
+        Stack<Character> stringStoreY = new Stack<>();
         //we gonna remove ab first and then ba
         //ab
         for(int i=0;i<s.length();i++){
             char curr = s.charAt(i);
-            if(stringStore.isEmpty()) stringStore.push(curr);
-            else if(curr=='b'&&stringStore.peek()=='a'){
+            ///work for stringStoreX
+            if(stringStoreX.isEmpty()) stringStoreX.push(curr);
+            else if(curr=='b'&&stringStoreX.peek()=='a'){
                 xFirst+=x;
-                stringStore.pop();
-            }else stringStore.push(curr);
-        }
-        StringBuilder newString  = new StringBuilder();
-        while (!stringStore.isEmpty()) newString.append(stringStore.pop());
-        newString = newString.reverse();
-        for(int i=0;i<newString.length();i++){
-            char curr = newString.charAt(i);
-            if(stringStore.isEmpty()) stringStore.push(curr);
-            else if(curr=='a'&&stringStore.peek()=='b'){
-                xFirst+=y;
-                stringStore.pop();
-            }else stringStore.push(curr);
-        }
+                stringStoreX.pop();
+            }else stringStoreX.push(curr);
 
-        //now we gonna remove ba first and then ab
-        stringStore = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char curr = s.charAt(i);
-            if(stringStore.isEmpty()) stringStore.push(curr);
-            else if(curr=='a'&&stringStore.peek()=='b'){
+            ///work for stringStoreY
+            if(stringStoreY.isEmpty()) stringStoreY.push(curr);
+            else if(curr=='a'&&stringStoreY.peek()=='b'){
                 yFirst+=y;
-                stringStore.pop();
-            }else stringStore.push(curr);
+                stringStoreY.pop();
+            }else stringStoreY.push(curr);
         }
-        newString  = new StringBuilder();
-        while (!stringStore.isEmpty()) newString.append(stringStore.pop());
-        newString = newString.reverse();
-        for(int i=0;i<newString.length();i++){
-            char curr = newString.charAt(i);
-            if(stringStore.isEmpty()) stringStore.push(curr);
-            else if(curr=='b'&&stringStore.peek()=='a'){
+        StringBuilder newStringX  = new StringBuilder();
+        StringBuilder newStringY  = new StringBuilder();
+        while (!stringStoreX.isEmpty()) newStringX.append(stringStoreX.pop());
+        while (!stringStoreY.isEmpty()) newStringY.append(stringStoreY.pop());
+        newStringX = newStringX.reverse();
+        newStringY = newStringY.reverse();
+        for(int i=0;i<newStringX.length();i++){
+            char curr = newStringX.charAt(i);
+            if(stringStoreX.isEmpty()) stringStoreX.push(curr);
+            else if(curr=='a'&&stringStoreX.peek()=='b'){
+                xFirst+=y;
+                stringStoreX.pop();
+            }else stringStoreX.push(curr);
+        }
+        for(int i=0;i<newStringY.length();i++){
+            char curr = newStringY.charAt(i);
+            if(stringStoreY.isEmpty()) stringStoreY.push(curr);
+            else if(curr=='b'&&stringStoreY.peek()=='a'){
                 yFirst+=x;
-                stringStore.pop();
-            }else stringStore.push(curr);
+                stringStoreY.pop();
+            }else stringStoreY.push(curr);
         }
         return Math.max(xFirst,yFirst);
     }
