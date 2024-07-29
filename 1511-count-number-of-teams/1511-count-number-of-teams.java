247d5a1e8d9671a4e108dcd1172085d0b52f1124
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2024-07-29 23:51:00 +0530

    Time: 18 ms (53.31%), Space: 42.6 MB (38.54%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -2,17 +2,17 @@ class Solution {
     public int numTeams(int[] rating) {
         int noOfTeams = 0;
         int n = rating.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if((rating[i]<rating[j])&&(rating[j]<rating[k])&&(rating[k]>rating[i])) {
-                        noOfTeams++;
-                    }
-                    if((rating[i]>rating[j])&&(rating[j]>rating[k])&&(rating[i]>rating[k])){
-                        noOfTeams++;
-                    } 
-                }
+        for(int j=0;j<n;j++){
+            int leftside=0,leftsidereverse=0,rightside=0,rightsidereverse=0;
+            for(int i=0;i<j;i++){
+                if(rating[i]<rating[j]) leftside++;
+                if(rating[i]>rating[j]) leftsidereverse++;
             }
+            for(int k=j+1;k<n;k++){
+                if(rating[j]<rating[k]) rightside++;
+                if(rating[j]>rating[k]) rightsidereverse++;
+            }
+            noOfTeams += leftside*rightside+leftsidereverse*rightsidereverse;
         }
         return noOfTeams;
     }
