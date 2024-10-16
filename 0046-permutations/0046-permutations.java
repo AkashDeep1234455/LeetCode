c91407c5293aabe9a3f760dd6c8f9f3350f790c8
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2024-10-17 00:41:38 +0530

    Time: 2 ms (46.04%), Space: 44.5 MB (34.23%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public static void helper(List<List<Integer>> num, List<Integer> temp, int[] nums) {
-        if (temp.size() == nums.length) {
-            num.add(new ArrayList<>(temp)); // Add a copy of temp
-        } else {
-            for (int i = 0; i < nums.length; i++) { // Iterate over nums.length
-                if (temp.contains(nums[i])) continue; // Skip if already included
-                temp.add(nums[i]); // Add the number to the current permutation
-                helper(num, temp, nums); // Recurse
-                temp.remove(temp.size() - 1); // Backtrack
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        helper(output,new ArrayList<>(),nums);
+        return output;
+    }
+    private void helper(List<List<Integer>> output,List<Integer> input,int nums[]){
+        if(input.size()==nums.length){
+            output.add(new ArrayList<>(input));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!input.contains(nums[i])){
+                input.add(nums[i]);
+                helper(output,input,nums);
+                input.remove(input.size()-1);
             }
         }
-    }
-
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> output = new ArrayList<>(); // Use ArrayList instead of LinkedList
-        helper(output, new ArrayList<>(), nums);
-        return output;
     }
 }
