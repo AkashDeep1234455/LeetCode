68b6172d69c5f8afeb0f9f11f01e136cd6c38773
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2024-10-17 00:29:43 +0530

    Time: 421 ms (8.59%), Space: 45 MB (13.4%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,28 +1,31 @@
 class Solution {
-     public static boolean helper(char[][]board,String word,int visited[][],int i,int j,int index){
-        if(index==word.length()) return true;
-        
-        if(i<0||j>=board[0].length||i>=board.length||j<0||visited[i][j]==1||board[i][j]!=word.charAt(index)){
-            return false;
-        }
-        visited[i][j] = 1;
-        boolean result = helper(board,word,visited,i+1,j,index+1)||helper(board,word,visited,i,j+1,index+1)||
-                helper(board,word,visited,i-1,j,index+1)||helper(board,word,visited,i,j-1,index+1);
-        visited[i][j] = 0;
-        return result;
-    }
+    private static int x[] = {1,-1,0,0};
+    private static int y[] = {0,0,1,-1};
+
     public boolean exist(char[][] board, String word) {
-        int rows=board.length;
-        int column=board[0].length;
-        int visited[][]=new int[rows][column];
-        boolean check=true;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<column;j++){
-                if(helper(board,word,visited,i,j,0)){
-                    return true;
+        int row = board.length;
+        int col = board[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j]==word.charAt(0)){
+                    boolean visited[][] = new boolean[row][col];
+                    boolean ans = helper(board,word,visited,i,j);
+                    if(ans) return true;
                 }
             }
         }
         return false;
+
+    }
+    private boolean helper(char[][]board, String word,boolean[][]visited,int oldX,int oldY){
+        if(word.length()==0){
+            return true;
+        }
+        if(oldX<0||oldY<0||oldX>=board.length||oldY>=board[0].length||visited[oldX][oldY]) return false;
+        if(board[oldX][oldY]!=word.charAt(0)) return false;
+        visited[oldX][oldY] = true;
+        boolean result = helper(board,word.substring(1),visited,oldX+1,oldY)||helper(board,word.substring(1),visited,oldX-1,oldY)||helper(board,word.substring(1),visited,oldX,oldY+1)||helper(board,word.substring(1),visited,oldX,oldY-1);
+        visited[oldX][oldY] = false;
+        return result;
     }
 }
