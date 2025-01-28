9836a7ca4a1b50c2465a4633b63b24108542f5c1
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-01-28 12:20:19 +0530

    Time: 6 ms (74.37%), Space: 46.5 MB (5.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,29 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean completed[] = new boolean[numCourses];
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        for(int []prerequisite:prerequisites){
-            int first = prerequisite[0];
-            int second = prerequisite[1];
-            if(map.containsKey(first)){
-                List<Integer> depend = map.get(first);
-                depend.add(second);
-            }else{
-                List<Integer> depend = new LinkedList<>();
-                depend.add(second);
-                map.put(first,depend);
-            }
-        }
+        HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
+        for (int[] prerequisite : prerequisites) {
+        graph.putIfAbsent(prerequisite[0], new ArrayList<>()); 
+        graph.get(prerequisite[0]).add(prerequisite[1]); 
+    }
+        int visited[] = new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            boolean visited[] = new boolean[numCourses];
-            if(!completed[i]){
-                boolean ans = helper(i,map,completed,visited);
-                if(!ans) return false;
+            if(visited[i]==0){
+            if(cycle(i,graph,visited)) return false;
             }
         }
         return true;
     }
-    public boolean helper(int course,HashMap<Integer,List<Integer>>map,boolean[] completed,boolean visited[]){
-        if(!map.containsKey(course)||completed[course]){
-            return true;
+    public boolean cycle(int start, HashMap<Integer,ArrayList<Integer>> graph,int[] visited){
+        if(visited[start]==1) return true;
+        if(visited[start]==2) return false;
+        visited[start] = 1;
+        if(graph.containsKey(start)){
+            ArrayList<Integer> nexts = graph.get(start);
+            for(int next:nexts){
+                if(cycle(next,graph,visited)) return true;
+            }
         }
-        if(visited[course]) return false;
-        visited[course] = true;
-        List<Integer> depend = map.get(course);
-        for(int i=0;i<depend.size();i++){
-            int val = depend.get(i);
-            boolean ans = helper(val,map,completed,visited);
-            if(ans) completed[val] = true;
-            if(!ans) return false;
-        }
-        return true;
-        
+        visited[start] = 2;
+        return false;
     }
 }
