c8720b7b1286dba20786564225fa8a3174397fa0
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-03-05 00:23:34 +0530

    Time: 1 ms (22.87%), Space: 42.1 MB (45.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,21 +1,11 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0,mid=0,high = nums.length-1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:nums){
+            pq.add(num);
+        }
         for(int i=0;i<nums.length;i++){
-            if(nums[mid]==0){
-                int temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
-                mid++;
-                low++;
-            }else if(nums[mid]==1){
-                mid++;
-            }else{
-                  int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
-                high--;
-            }
+            nums[i] = pq.poll();
         }
     }
 }
