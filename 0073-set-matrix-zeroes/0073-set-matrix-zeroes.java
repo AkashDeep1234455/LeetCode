495c7fb4af06ed0512e68b0f1e23ace0fbd76415
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-03-05 00:09:20 +0530

    Time: 1 ms (75.7%), Space: 45.5 MB (82.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,50 +1,36 @@
 class Solution {
+    public void valueChanger(int [][] matrix,int x,int y){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0;i<x;i++){
+            matrix[i][y]=0;
+        }
+        for(int i=x+1;i<m;i++){
+            matrix[i][y]=0;
+        }
+        for(int j=0;j<y;j++){
+            matrix[x][j]=0;
+        }
+        for(int j=y+1;j<n;j++){
+            matrix[x][j]=0;
+        }
+    }
+    
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-
-        boolean firstRowZero = false;
-        boolean firstColumnZero = false;
-
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColumnZero = true;
-                break;
-            }
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        List<int[]> zeroIndex = new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    int[] entry = new int[]{i,j};
+                    zeroIndex.add(entry);
                 }
             }
         }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if (firstColumnZero) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
+        for(int i=0;i<zeroIndex.size();i++){
+            int[] curr = zeroIndex.get(i);
+            valueChanger(matrix,curr[0],curr[1]);
         }
     }
 }
