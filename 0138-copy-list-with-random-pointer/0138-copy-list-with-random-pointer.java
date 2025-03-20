b70f30ffcab21337a9d3326bff333f6d4fa11334
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-03-20 22:33:34 +0530

    Time: 3 ms (1.48%), Space: 44.8 MB (29.52%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,42 +15,40 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-         HashMap<Integer,Node> map = new HashMap<>();
-        Node copyHead = head;
-        int i = 0;
-        while (copyHead!=null){
-            map.put(i,copyHead);
+        HashMap<Integer,Node> map = new HashMap<>();
+        Node temp = head;
+        int i=0;
+        while(temp!=null){
+            map.put(i,temp);
+            temp=temp.next;
             i++;
-            copyHead = copyHead.next;
         }
-        copyHead = head;
-        HashMap<Integer,Node> output = new HashMap<>();
+        temp = head;
+        HashMap<Integer,Node> map2 = new HashMap<>();
         for(int key:map.keySet()){
             Node input = new Node(map.get(key).val);
-            if(key==0) {
-                output.put(key,input);
+            if(key==0){
+                map2.put(key,input);
                 continue;
             }
-            output.get(key-1).next = input;
+            map2.get(key-1).next = input;
             if(key==i){
                 input.next = null;
             }
-            output.put(key,input);
-
+            map2.put(key,input);
         }
         for(int key:map.keySet()){
             Node curr = map.get(key);
             Node random = curr.random;
             int index = 0;
-            copyHead = head;
-            while (copyHead!=random){
-                copyHead = copyHead.next;
+            temp=head;
+            while(temp!=random){
+                temp=temp.next;
                 index++;
             }
-            Node outputCurr = output.get(key);
-            outputCurr.random = output.get(index);
+            Node currOutput = map2.get(key);
+            currOutput.random = map2.get(index);
         }
-
-        return output.get(0);
+        return map2.get(0);
     }
 }
