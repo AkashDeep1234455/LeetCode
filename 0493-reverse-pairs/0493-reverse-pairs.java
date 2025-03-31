77697c9854d1981cd98e40620a451ec2d6584143
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-04-01 00:41:23 +0530

    Time: 40 ms (91.48%), Space: 55 MB (55.88%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,35 +1,36 @@
 class Solution {
     public int reversePairs(int[] nums) {
-        if(nums==null||nums.length==0) return 0;
+        if(nums.length==0||nums==null) return 0;
         return mergeSort(nums,0,nums.length-1);
     }
-    public int mergeSort(int[]nums,int start,int end){
-        if(start>=end) return 0;
-        int mid = start + (end - start) / 2;
-        int count = mergeSort(nums,start,mid)+mergeSort(nums,mid+1,end);
-        ///count
-        int j=mid+1;
-        for(int i=start;i<=mid;i++){
-            while(j<=end&&(long)nums[i]>2L*nums[j]){
+    public int mergeSort(int []nums,int left,int right){
+        if(left>=right) return 0;
+        int mid = left +(right-left)/2;
+        int count = mergeSort(nums,left,mid)+mergeSort(nums,mid+1,right);
+
+        ///counting
+        int j = mid+1;
+        for(int i=left;i<=mid;i++){
+            while(j<=right&&(long)nums[i]>2L*nums[j]){
                 j++;
             }
             count+=(j-mid-1);
         }
-        merge(nums,start,mid,end);
+        merge(nums,left,mid,right);
         return count;
     }
     public void merge(int[]nums,int left,int mid,int right){
-        int temp []=new int[right-left+1];
+        int temp[] = new int[right-left+1];
         int i=left,j=mid+1,k=0;
         while(i<=mid&&j<=right){
             if(nums[i]>nums[j]){
-                temp[k++] = nums[j++];
+                temp[k++] =nums[j++];
             }else{
                 temp[k++] = nums[i++];
             }
         }
-        while (i <= mid) temp[k++] = nums[i++];
-        while (j <= right) temp[k++] = nums[j++];
-        System.arraycopy(temp, 0, nums, left, temp.length);
+        while(j<=right) temp[k++] =nums[j++];
+        while(i<=mid) temp[k++] =nums[i++];
+        System.arraycopy(temp,0,nums,left,temp.length);
     }
 }
