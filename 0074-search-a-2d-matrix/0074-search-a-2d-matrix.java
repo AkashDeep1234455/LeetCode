9498433a17f15982e657be1fcfd0efbf2f885cc6
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-03-31 21:58:57 +0530

    Time: 0 ms (100%), Space: 42 MB (91.38%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,25 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-        if(matrix[0][0]>target || matrix[m-1][n-1]<target) return false;
-        int rowToSearch = 0;
         for(int i=0;i<m;i++){
-            int start = matrix[i][0];
-            int end = matrix[i][n-1];
-            if(target>=start&&target<=end){
-                rowToSearch = i;
+            if(matrix[i][n-1]==target) return true;
+            else if(matrix[i][n-1]>target){
+                row = i;
                 break;
             }
         }
-        System.out.println(rowToSearch);
-        int arrToSearch[] = matrix[rowToSearch];
         int start = 0;
         int end = n-1;
-        while(start<=end){
+        while(start<end){
             int mid = (start+end)/2;
-            if(arrToSearch[mid]==target||arrToSearch[start]==target||arrToSearch[end]==target) return true;
-            else if(arrToSearch[mid]>target){
+            if(matrix[row][mid]==target||matrix[row][start]==target||matrix[row][end]==target) return true;
+            else if(matrix[row][mid]>target){
                 end = mid-1;
             }else{
                 start = mid+1;
