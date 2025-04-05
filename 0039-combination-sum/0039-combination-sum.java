5787215d1c4629417a8d25f0e3979b5f54edb19d
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-04-06 00:16:16 +0530

    Time: 395 ms (5.16%), Space: 45.6 MB (5.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> output = new ArrayList<>();
-        helper(output,new ArrayList(),target,new int[1],candidates);
-        return output;
-    }
-    private void helper(List<List<Integer>> output,List<Integer>input,int target,int currSum[],int candidates[]){
-        if(currSum[0]==target){
-            List<Integer> newList = new ArrayList<>(input);
-            Collections.sort(newList);
-            if(!output.contains(newList)){
-                output.add(newList);
+    public void help(List<List<Integer>> output,int nums[],int sum[],int target,List<Integer> input){
+        if(sum[0]==target){
+            LinkedList<Integer> entry = new LinkedList<>(input);
+            Collections.sort(entry);
+            if(!output.contains(entry)){
+                output.add(new LinkedList<>(entry));
             }
             return;
         }
-        if(currSum[0]>target) return;
-        for(int i=0;i<candidates.length;i++){
-                currSum[0]+=candidates[i];
-                input.add(candidates[i]);
-                helper(output,input,target,currSum,candidates);
-                input.remove(input.size()-1);
-                currSum[0]-=candidates[i];
+        if(sum[0]>target) return;
+        for(int i=0;i<nums.length;i++){
+            input.add(nums[i]);
+            sum[0]+=nums[i];
+            help(output,nums,sum,target,input);
+            input.remove(input.size()-1);
+            sum[0]-=nums[i];
         }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> output = new LinkedList<>();
+        help(output,candidates,new int[1],target,new LinkedList<>());
+        return output;
     }
 }
