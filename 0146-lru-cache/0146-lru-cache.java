c727ca93e7cc8504837cb4287f25d0c51e879e46
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-04-06 18:53:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,37 +1,35 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-
 class LRUCache {
-    private int capacity;
-    private HashMap<Integer, Integer> map;
-    private LinkedList<Integer> recentlyUsed;
-
+    int capacity;
+    HashMap<Integer,Integer> map;
+    LinkedList<Integer> recentlyUsed;
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.map = new HashMap<>();
-        this.recentlyUsed = new LinkedList<>();
+        map = new HashMap<>();
+        recentlyUsed = new LinkedList<>();
     }
-
+    
     public int get(int key) {
-        if (map.containsKey(key)) {
-            recentlyUsed.remove(Integer.valueOf(key)); // Remove by value
-            recentlyUsed.add(key);
-            return map.get(key);
+        if(map.containsKey(key)){
+        int index = recentlyUsed.indexOf(key);
+        recentlyUsed.remove(index);
+        recentlyUsed.add(key);
+        return map.get(key);
         }
         return -1;
     }
-
+    
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            map.put(key, value);
-            recentlyUsed.remove(Integer.valueOf(key)); // Remove by value
+        if(map.containsKey(key)){
+            map.put(key,value);
+            int index = recentlyUsed.indexOf(key);
+            recentlyUsed.remove(index);
             recentlyUsed.add(key);
-        } else {
-            if (map.size() >= capacity) {
+        }else {
+            if(map.size() >= capacity){
                 int leastRecentlyUsedKey = recentlyUsed.removeFirst();
                 map.remove(leastRecentlyUsedKey);
             }
-            map.put(key, value);
+            map.put(key,value);
             recentlyUsed.add(key);
         }
     }
@@ -41,5 +39,5 @@ class LRUCache {
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
  * int param_1 = obj.get(key);
- * obj.put(key, value);
+ * obj.put(key,value);
  */
