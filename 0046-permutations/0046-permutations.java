b8e91aa7aa9be7e4028fceb52279a15159582e2e
Akash Deep <118648099+AkashDeep1234455@users.noreply.github.com>
2025-04-10 17:15:51 +0530

    Time: 1 ms (93.18%), Space: 44.6 MB (64.74%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,20 +1,23 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> output = new ArrayList<>();
-        helper(output,new ArrayList<>(),nums);
-        return output;
-    }
-    private void helper(List<List<Integer>> output,List<Integer> input,int nums[]){
-        if(input.size()==nums.length){
+    public void helper(int[]nums,HashSet<Integer> visited,List<List<Integer>> output,List<Integer> input,int n){
+        if(input.size()==n){
             output.add(new ArrayList<>(input));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!input.contains(nums[i])){
+        for(int i=0;i<n;i++){
+            if(!visited.contains(i)){
+                visited.add(i);
                 input.add(nums[i]);
-                helper(output,input,nums);
+                helper(nums,visited,output,input,n);
+                visited.remove(i);
                 input.remove(input.size()-1);
             }
         }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        helper(nums,new HashSet<>(),output,new ArrayList<>(),nums.length);
+        return output;
+
     }
 }
